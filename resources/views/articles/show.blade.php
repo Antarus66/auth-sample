caa596911a0eee5013d3e3fefc2375a4ebed34b0
antarus66 <antarus66@gmail.com>
2018-05-29 22:43:34 +0300

    Added a markup for article show.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,2 +1,18 @@
-<p>{{ $article->title }}</p>
-<p>{{ $article->text }}</p>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $article->title }}
+                        <span class="float-right">belongs to user {{ $article->user_id }}</span>
+                    </div>
+
+                    <div class="card-body">{{ $article->text }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
